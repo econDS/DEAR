41d249dd81d43393a0e666a06cb4e1d6f0ce1a96
econDS <35423537+econDS@users.noreply.github.com>
2018-11-16 22:40:13 +0700

    add distribution diagnosis plot

---
+++ R/dist_diag.R
@@ -0,0 +1,17 @@
+#' dist_diag
+#'
+#' @param x a numerical vector
+#' @param title title of plot
+#'
+#' @return histogram and box plot
+#' @export
+#'
+#' @examples
+#' dist_diag(mtcars$mpg)
+dist_diag <- function(x, title=deparse(substitute(x))) {
+  par(mfrow=c(1,2), oma = c(0, 0, 1.7, 0))
+  hist(x, main = "Histogram", xlab = "")
+  boxplot(x, main="Boxplot", horizontal = T)
+  mtext(title, outer = TRUE, cex = 1.5)
+  par(mfrow=c(1,1), oma = c(0, 0, 0, 0))
+}
