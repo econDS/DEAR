20eb0f482d671ec76f228eaa8ffa13168da0f86e
econDS <35423537+econDS@users.noreply.github.com>
2018-11-16 22:32:53 +0700

    change argument naming add default value for titile

--- R/pielab.R
+++ R/pielab.R
@@ -3,9 +3,9 @@
 #' create a pie chart with percentage label.
 #'
 #' @param x a vector of categories variable
-#' @param lb a label of each category
-#' @param cl a vector of colors
-#' @param tt a title of plot
+#' @param label a label of each category
+#' @param col a vector of colors
+#' @param titile a title of plot
 #' @param center start division line at center
 #' @param fade make the pie chart fade
 #'
@@ -15,33 +15,33 @@
 #' @examples
 #' a <- c("A","A","A","B","B","C")
 #' pielab(a)
-pielab <- function(x, lb="default", cl="default", tt="", center=FALSE ,fade=FALSE) {
+pielab <- function(x, label="default", col="default", title=deparse(substitute(x)), center=FALSE ,fade=FALSE) {
   ft <- factor(x)
   pct <- round(table(ft)*100/sum(table(ft)), digits = 1)
 
-  if(lb=="default"){
-    lb <- unique(ft)
-    lb <- paste(lb, pct)
-    lb <- paste(lb,"%",sep="")
+  if(label=="default"){
+    label <- unique(ft)
+    label <- paste(label, pct)
+    label <- paste(label,"%",sep="")
   } else {
-    lb <- paste(lb, pct)
-    lb <- paste(lb,"%",sep="")
+    label <- paste(label, pct)
+    label <- paste(label,"%",sep="")
   }
 
-  if(cl=="default"){
+  if(col=="default"){
     color_set <- c("#A6CEE3","#1F78B4","#B2DF8A","#33A02C",
                    "#FB9A99","#E31A1C","#FDBF6F","#FF7F00")
-    cl <- color_set[1:length(unique(ft))]
+    col <- color_set[1:length(unique(ft))]
   }
 
   if (center==TRUE & fade==TRUE){
-    pie(table(ft), col=cl, labels = lb, main=tt, init.angle = 270, density=50)
+    pie(table(ft), col=col, labels = label, main=title, init.angle = 270, density=50)
   } else if (center==TRUE & fade==FALSE) {
-    pie(table(ft), col=cl, labels = lb, main=tt, init.angle = 270)
+    pie(table(ft), col=col, labels = label, main=title, init.angle = 270)
   } else if (center==FALSE & fade==TRUE) {
-    pie(table(ft), col=cl, labels = lb, main=tt, density=50)
+    pie(table(ft), col=col, labels = label, main=title, density=50)
   } else {
-    pie(table(ft), col=cl, labels = lb, main=tt)
+    pie(table(ft), col=col, labels = label, main=title)
   }
   freqeuncy_table <- ft
   return(table(freqeuncy_table))
